0d452a79361b4cba16d74e39581a15be1f3fa89c
Randy Girard <rgirard59@yahoo.com>
2016-05-18 12:57:49 -0400

    Added time consuming transactions

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -9,6 +9,14 @@ class OverviewController < ApplicationController
       .joins(:transaction_sample_data)
       .group("transaction_endpoints.id")
       .having("COUNT(transaction_sample_data.id) > 0")
+
+    @time_consuming_transactions = @application
+      .transaction_endpoints
+      .select("transaction_endpoints.*, SUM(transaction_data.call_count * transaction_data.duration) AS total_time_spent")
+      .joins(:transaction_data)
+      .group("transaction_endpoints.id")
+      .order("SUM(transaction_data.call_count * transaction_data.duration) DESC")
+    @total_time_spent = @time_consuming_transactions.map(&:total_time_spent).sum
   end
 
   private
