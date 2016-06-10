a2824b56c4f4d07488f4013fb52d58a45c834ff0
Randy Girard <rgirard59@yahoo.com>
2016-06-10 15:41:42 -0400

    Average based on # of transactions, not calls

--- app/reporters/duration_reporter.rb
+++ app/reporters/duration_reporter.rb
@@ -11,11 +11,11 @@ class DurationReporter < Reporter
     end
 
     hash = []
-    hash.push({ :name => "View", :data => data.group_by_period(*report_params).calculate_all("CASE SUM(view_call_count) WHEN 0 THEN 0 ELSE SUM(view_duration) / SUM(view_call_count) END") }) rescue nil
-    hash.push({ :name => "Database", :data => data.group_by_period(*report_params).calculate_all("CASE SUM(db_call_count) WHEN 0 THEN 0 ELSE SUM(db_duration) / SUM(db_call_count) END") }) rescue nil
-    hash.push({ :name => "App", :data => data.group_by_period(*report_params).calculate_all("CASE SUM(app_call_count) WHEN 0 THEN 0 ELSE SUM(app_duration) / SUM(app_call_count) END") }) rescue nil
-    hash.push({ :name => "Middleware", :data => data.group_by_period(*report_params).calculate_all("CASE SUM(middleware_call_count) WHEN 0 THEN 0 ELSE SUM(middleware_duration) / SUM(middleware_call_count) END") }) rescue nil
-    hash.push({ :name => "GC Execution", :data => data.group_by_period(*report_params).calculate_all("CASE SUM(gc_call_count) WHEN 0 THEN 0 ELSE SUM(gc_duration) / SUM(gc_call_count) END") }) rescue nil
+    hash.push({ :name => "View", :data => data.group_by_period(*report_params).calculate_all("CASE COUNT(*) WHEN 0 THEN 0 ELSE SUM(view_duration) / COUNT(*) END") }) rescue nil
+    hash.push({ :name => "Database", :data => data.group_by_period(*report_params).calculate_all("CASE COUNT(*) WHEN 0 THEN 0 ELSE SUM(db_duration) / COUNT(*) END") }) rescue nil
+    hash.push({ :name => "App", :data => data.group_by_period(*report_params).calculate_all("CASE COUNT(*) WHEN 0 THEN 0 ELSE SUM(app_duration) / COUNT(*) END") }) rescue nil
+    hash.push({ :name => "Middleware", :data => data.group_by_period(*report_params).calculate_all("CASE COUNT(*) WHEN 0 THEN 0 ELSE SUM(middleware_duration) / COUNT(*) END") }) rescue nil
+    hash.push({ :name => "GC Execution", :data => data.group_by_period(*report_params).calculate_all("CASE COUNT(*) WHEN 0 THEN 0 ELSE SUM(gc_duration) / COUNT(*) END") }) rescue nil
     hash
   end
 
