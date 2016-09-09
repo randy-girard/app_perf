cef9d2b5d27c41bf210f1154245114ecb2eec495
randy-girard <rgirard59@yahoo.com>
2016-09-09 09:21:19 -0400

    Change to use env value.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server 'appperf.freezzo.com', :user => "rails", :roles => %w{app db web}
+server ENV["deploy_server"], :user => "rails", :roles => %w{app db web}
 
 set :deploy_to, '/home/rails/apps/app_perf'
 
