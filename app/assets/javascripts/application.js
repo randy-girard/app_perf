7d72e90409c8b7f1b2240d32073d36a350024d92
Randy Girard <rgirard59@gmail.com>
2019-07-26 11:59:08 -0400

    Fix issue with deleting resoruces from views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
 //= require activestorage
 //= require jquery
 //= require bootstrap-sprockets
