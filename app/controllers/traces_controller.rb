52a566bd38ac72b23e3859c94267daf7aed1db8e
Randy Girard <rgirard59@gmail.com>
2020-07-13 11:41:04 -0400

    Handle nil trace issue for now.

--- app/controllers/traces_controller.rb
+++ app/controllers/traces_controller.rb
@@ -18,46 +18,50 @@ class TracesController < ApplicationController
       .where(:trace_key => params[:id])
       .first
 
-    @spans = @trace.spans.sort_by(&:timestamp)
-    @layer_ids = @spans.map(&:layer_id).uniq
-    @layer_names = Layer.where(id: @layer_ids).pluck(:name)
-    @span = @spans.find {|s| s.id == params[:span_id] }
+    if @trace.nil?
+      redirect_to application_traces_url(@current_application)
+    else
+      @spans = @trace.spans.sort_by(&:timestamp)
+      @layer_ids = @spans.map(&:layer_id).uniq
+      @layer_names = Layer.where(id: @layer_ids).pluck(:name)
+      @span = @spans.find {|s| s.id == params[:span_id] }
 
-    @database_calls = @current_application
-      .database_calls
-      .joins(:span)
-      .where(:spans => { :id => @trace.spans.select(:id) })
+      @database_calls = @current_application
+        .database_calls
+        .joins(:span)
+        .where(:spans => { :id => @trace.spans.select(:id) })
 
-    group_index = 0
-    item_index = 0
-    @groups = []
-    @items = []
-    @spans.group_by {|s| [s.application, s.layer.name] }.each_pair do |(application, layer_name), spans|
+      group_index = 0
+      item_index = 0
+      @groups = []
+      @items = []
+      @spans.group_by {|s| [s.application, s.layer.name] }.each_pair do |(application, layer_name), spans|
 
-      content = []
-      if @current_application.name != application.name
-        content << view_context.link_to(application.name, dynamic_url(application, :trace, id: @trace.trace_key))
-      end
-      content << layer_name
-
-      @groups << { :id => group_index, :content => content.join(": "), :value => group_index + 1 }
-      spans.each do |span|
-        if layer_name == "rack-middleware"
-          duration = span.exclusive_duration
-        else
-          duration = span.duration
+        content = []
+        if @current_application.name != application.name
+          content << view_context.link_to(application.name, dynamic_url(application, :trace, id: @trace.trace_key))
         end
-        @items << {
-          :id => span.id,
-          :group => group_index,
-          :content => "",
-          :start => span.timestamp.to_f * 1000,
-          :end => (span.timestamp.to_f * 1000) + duration.to_f,
-          :className => "app-perf-color-#{layer_name}#{span.has_error? ? " span-error" : ""}"
-        }
-        item_index += 1
+        content << layer_name
+
+        @groups << { :id => group_index, :content => content.join(": "), :value => group_index + 1 }
+        spans.each do |span|
+          if layer_name == "rack-middleware"
+            duration = span.exclusive_duration
+          else
+            duration = span.duration
+          end
+          @items << {
+            :id => span.id,
+            :group => group_index,
+            :content => "",
+            :start => span.timestamp.to_f * 1000,
+            :end => (span.timestamp.to_f * 1000) + duration.to_f,
+            :className => "app-perf-color-#{layer_name}#{span.has_error? ? " span-error" : ""}"
+          }
+          item_index += 1
+        end
+        group_index += 1
       end
-      group_index += 1
     end
   end
 
